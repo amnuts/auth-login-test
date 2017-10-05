ed9076318afaa1b29ce744718c14cc1b418c1eac
Andrew Collington <andy@amnuts.com>
2017-10-05 16:24:42 +0100

    Authenticate route added.

--- service-auth/src/routes.php
+++ service-auth/src/routes.php
@@ -3,12 +3,38 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use Lcobucci\JWT\Signer\Hmac\Sha512;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Builder;
+
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->post('/authenticate', function (Request $request, Response $response, array $args) {
+    $username = $request->getParam('username');
+    $password = $request->getParam('password');
+    if (empty($username) || empty($password)) {
+        return $response->withJson(['error' => 'Username or password not supplied']);
+    }
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $user = $this->db->querySingle("select * from users where username = '" . $this->db->escapeString($username) . "'", true);
+    if (empty($user) || $user['password'] != password_hash($password, PASSWORD_DEFAULT)) {
+        return $response->withJson(['error' => 'Username or password is incorrect']);
+    }
+
+    $token = (new Builder())
+        ->setAudience('http://localhost:8010')
+        ->setIssuer('http://localhost:8020')
+        ->setId(uniqid())
+        ->setIssuedAt((new \DateTime('now'))->getTimestamp())
+        ->setNotBefore((new \DateTime('now'))->getTimestamp())
+        ->set('user', (object)[
+            'id' => $user['id'],
+            'email' => $user['email'],
+            'firstname' => $user['firstname'],
+            'surname' => $user['surname']
+        ])
+        ->sign(new Sha512(), new Key(file_get_contents(__DIR__.'/../../hs512')))
+        ->getToken();
+
+    return $response->withJson(['token' => (string)$token]);
 });
